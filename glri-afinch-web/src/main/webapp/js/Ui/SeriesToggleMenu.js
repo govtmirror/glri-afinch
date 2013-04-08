5e0e586223f47ea8f001582645c42830d4be4127
Carl Schroedl <cschroedl@usgs.gov>
2013-04-08 16:02:23 -0500

    first pass at implementing detection of only-monthly series.

--- glri-afinch-web/src/main/webapp/js/Ui/SeriesToggleMenu.js
+++ glri-afinch-web/src/main/webapp/js/Ui/SeriesToggleMenu.js
@@ -1,53 +1,100 @@
 Ext.ns("AFINCH.ui");
 AFINCH.ui.SeriesToggleMenu= Ext.extend(Ext.menu.Menu, {
+    /**
+     * Maintain a map of series names to CheckItem objects.
+     * Use series names instead of the button text because user may want to change
+     * button text.
+     */
+    checkedSeriesButtons: {},
+    monthlySeriesIds: ['mean_monthly_flow', 'median_monthly_flow'], //matches against programmatic identifiers, not user-facing text.
     getSeriesTogglers: function(){
       return this.items.items;  
     },
-    constructor: function(config) {
-         var toggleSeriesHandler = function(checkItem, checked, optionalGraph){
-            var graph;
-            if(optionalGraph){
-                graph = optionalGraph;
-            }else{
-                var win = checkItem.findParentByType('dataWindow');
-                graph = win.graphPanel.graph;
-            } 
+    toggleSeriesHandler: function(checkItem, checked, optionalGraph){
+        var self = this;
+        
+        //update the seriesIdToStats map
+        if(checked){
+            self.checkedSeriesButtons[checkItem.seriesId] = checkItem;
+        }
+        else{
+            delete self.checkedSeriesButtons[checkItem.seriesId];
+        }
+        
+        var checkedSeriesIds = Object.keys(self.checkedSeriesButtons);
+        
+        var isAMonthlySeriesId = function(id){
+            return id in self.monthlySeriesIds;
+        };
+        
+        if( (   //if the set of currently checked series is equivalent to the set of monthly series
+            checkedSeriesIds.length === self.monthlySeriesIds.length
+            && Array.every(checkedSeriesIds, isAMonthlySeriesId)
+            )
+                
+            ||
             
-            if(checkItem.chartColumn !== undefined || checkItem.statName){
-                if(checkItem.statName == 'deciles'){
-                    for(var i = 5; i < 14; i++){
-                        graph.setVisibility(i, checked);
-                    }
-                }
-                else{
-                    graph.setVisibility(checkItem.chartColumn, checked);
+            ( //or if the set of currently checked series is a subset of the set of monthly series
+            checkedSeriesIds.length < self.monthlySeriesIds.length
+            && Array.every(checkedSeriesIds, isAMonthlySeriesId)
+            )    
+           ){
+            
+            alert("only monthly series are selected");
+        }
+        
+        
+        var graph;
+        if(optionalGraph){
+            graph = optionalGraph;
+        }else{
+            var win = checkItem.findParentByType('dataWindow');
+            graph = win.graphPanel.graph;
+        } 
+
+
+        if(checkItem.chartColumn !== undefined || checkItem.seriesId){
+            if(checkItem.seriesId == 'deciles'){
+                for(var i = 5; i < 14; i++){
+                    graph.setVisibility(i, checked);
                 }
             }
-        };
+            else{
+                graph.setVisibility(checkItem.chartColumn, checked);
+            }
+        }
+
+    },
+    constructor: function(config) {
+        var self = this;
         
         //compose some CheckItem objects
         var baseCheckItem = {
-            checkHandler: toggleSeriesHandler,
+            checkHandler: self.toggleSeriesHandler,
             checked: false,
             disabled: true
         }
-        //most statName fields not in use EXCEPT for deciles
+        
+        //in the following composed buttons...
+        //'seriesId' properties are for programmatic identification of buttons
+        //whereas 'text' properties are for display to the user
         var checkItems = [
         {
             text: 'Monthly Flow',
+            seriesId: 'monthly_flow',
             chartColumn: 0,
             checked: true
         },
         {
             text: 'Mean Annual Flow',
-            statName: 'mean_annual_flow',
+            seriesId: 'mean_annual_flow',
             chartColumn: 1,
             checked: true
         }, 
 
         {
             text:'Median Annual Flow',
-            statName: 'median_annual_flow',
+            seriesId: 'median_annual_flow',
             chartColumn: 2,
             checked: true
         }, 
@@ -55,17 +102,17 @@ AFINCH.ui.SeriesToggleMenu= Ext.extend(Ext.menu.Menu, {
         {
             text: 'Mean Monthly Flow',
             chartColumn: 3,
-            statName: 'mean_monthly_flow'
+            seriesId: 'mean_monthly_flow'
         },
 
         {
             text: 'Median Monthly Flow',
-            statName: 'median_monthly_flow',
+            seriesId: 'median_monthly_flow',
             chartColumn: 4
         },
         {
             text: 'Deciles',
-            statName: 'deciles'
+            seriesId: 'deciles'
         //no chart column because deciles is a special case
         }
         ];
@@ -74,7 +121,14 @@ AFINCH.ui.SeriesToggleMenu= Ext.extend(Ext.menu.Menu, {
             Object.merge(composedCheckItem, baseCheckItem);
             return Object.merge(composedCheckItem, checkItem);
         });
-            
+        
+        //now update seriesIdsCheckedStatus map
+        checkItems.each(function(checkItem){
+            if(checkItem.checked){
+                self.checkedSeriesButtons[checkItem.seriesId] = checkItem;
+            }
+        });
+        
         config = Ext.apply({
             items : checkItems
         }, config);
