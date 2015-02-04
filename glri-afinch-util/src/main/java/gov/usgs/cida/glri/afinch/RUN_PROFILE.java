fd7a20bbe790afbd9b3ffe65152dd84459195905
Eric Everman <eeverman@usgs.gov>
2015-02-04 13:05:28 -0600

    Some changes to simplify running the utility from command line, as well as better names for some of the options.

--- glri-afinch-util/src/main/java/gov/usgs/cida/glri/afinch/RUN_PROFILE.java
+++ glri-afinch-util/src/main/java/gov/usgs/cida/glri/afinch/RUN_PROFILE.java
@@ -5,6 +5,43 @@ package gov.usgs.cida.glri.afinch;
  * @author eeverman
  */
 public enum RUN_PROFILE {
-	FLOW,
-	CATCH;
+	FLOW("Flowlines", "flow_out", "ComID", "QAccCon", "QAC"),
+	CATCH("Catchments", "catch_out", "GridCode", "yieldCatchCon", "YCC");
+	
+	private final String srcSubDirectoryName;
+	private final String outSubDirectoryName;
+	private final String idColumn;
+	private final String dataColumnPrefix;
+	private final String abbreviation;
+	
+	
+	private RUN_PROFILE(String srcSubDirectoryName, String outSubDirectoryName, String idColumn, String dataColumnPrefix, String abbreviation) {
+		this.srcSubDirectoryName = srcSubDirectoryName;
+		this.outSubDirectoryName = outSubDirectoryName;
+		this.idColumn = idColumn;
+		this.dataColumnPrefix = dataColumnPrefix;
+		this.abbreviation = abbreviation;
+	}
+
+	public String getDefaultSrcSubDirectoryName() {
+		return srcSubDirectoryName;
+	}
+
+	public String getDefaultOutSubDirectoryName() {
+		return outSubDirectoryName;
+	}
+
+	public String getDefaultIdColumn() {
+		return idColumn;
+	}
+
+	public String getDefaultDataColumnPrefix() {
+		return dataColumnPrefix;
+	}
+
+	public String getDefaultAbbr() {
+		return abbreviation;
+	}
+	
+	
 }
